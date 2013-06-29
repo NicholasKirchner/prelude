d5c69b67741125ded7fe079e99a89c6e5389c1f6
Nicholas Kirchner <kirc0076@umn.edu>
2013-06-29 16:52:21 -0700

    removed extraneous comments

--- personal/extra-packages.el
+++ personal/extra-packages.el
@@ -1,12 +1,3 @@
-;;;Need the following packages:
-;; 2. Something for rails (found rinari... won't install thru elpa/marm...)
-;; 5. nxhtml
-
-;; rinari is problematic right now since the install instructions
-;; installed an alternate version of inf-ruby
-
-;; nxhtml is not emacs24 compliant yet.
-
 (defvar extra-packages
   '(gnuplot ess rvm rinari w3m sr-speedbar csharp-mode csv-mode rhtml-mode)
   "A further list of packages to ensure are installed at launch.")
